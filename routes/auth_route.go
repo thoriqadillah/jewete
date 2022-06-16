39a7b8a958529670bee381532d43a56b40629090
thoriqadillah <thoriqadillah59@gmail.com>
2022-06-16 16:43:51 +0700

    set ruting for auth

---
+++ routes/auth_route.go
@@ -0,0 +1,15 @@
+package routes
+
+import (
+	"jewete/controllers"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func authRoute(auth fiber.Router) {
+	auth.Post("/register", controllers.Register)
+	auth.Post("/login", controllers.Login)
+	// auth.Post("/refresh", controllers.RefreshToken)
+	// auth.Post("/logout", controllers.Logout)
+
+}
---
+++ routes/router.go
@@ -0,0 +1,11 @@
+package routes
+
+import (
+	"github.com/gofiber/fiber/v2"
+)
+
+func Setup(app *fiber.App) {
+	v1 := app.Group("/api/v1")
+
+	v1.Route("/auth", authRoute)
+}
