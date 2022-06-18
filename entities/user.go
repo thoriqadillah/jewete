b2c0fb35c757191bdc42a599f890620f0fafd11c
thoriqadillah <thoriqadillah59@gmail.com>
2022-06-18 18:49:45 +0700

    only show the important data

--- entities/user.go
+++ entities/user.go
@@ -7,11 +7,11 @@ import (
 )
 
 type User struct {
-	ID        uint   `json:"id" gorm:"primaryKey"`
-	Username  string `json:"username" gorm:"unique,not null"`
-	Email     string `json:"email" gorm:"unique,not null"`
-	Password  string `json:"-" gorm:"not null"`
-	CreatedAt time.Time
-	UpdatedAt time.Time
-	DeletedAt gorm.DeletedAt `gorm:"index"`
+	ID        uint           `json:"id" gorm:"primaryKey"`
+	Username  string         `json:"username" gorm:"unique,not null"`
+	Email     string         `json:"email" gorm:"unique,not null"`
+	Password  string         `json:"-" gorm:"not null"`
+	CreatedAt time.Time      `json:"-"`
+	UpdatedAt time.Time      `json:"-"`
+	DeletedAt gorm.DeletedAt `json:"-" gorm:"index"`
 }
