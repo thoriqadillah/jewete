598c216714232644b20cadcc016b694c936994db
thoriqadillah <thoriqadillah59@gmail.com>
2022-06-18 18:38:53 +0700

    setup cors

--- main.go
+++ main.go
@@ -5,11 +5,16 @@ import (
 	"jewete/routes"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func main() {
 	database.GetInstance()
 	app := fiber.New()
+
+	app.Use(cors.New(cors.Config{
+		AllowCredentials: true,
+	}))
 
 	routes.Setup(app)
 
